79d0ca7156cd2b93ee4069fa4c08253804ef91b1
linph <370345882@qq.com>
2018-09-21 17:13:17 +0800

    init vuepress

---
+++ vuepress/doc/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': 'path/to/some/dir'
+      }
+    }
+  }
+}
